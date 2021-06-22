8c873374a7ad7412d1e479f304e13a68ff9e8436
mabus <m.buscaroli@outlook.com>
2021-06-22 22:23:34 +0100

    Initial Commit

---
+++ myShifts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  myShifts
+//
+//  Created by Matteo on 22/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ myShifts/myShiftsApp.swift
@@ -0,0 +1,17 @@
+//
+//  myShiftsApp.swift
+//  myShifts
+//
+//  Created by Matteo on 22/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct myShiftsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
